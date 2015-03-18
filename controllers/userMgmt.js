027e5dd4969441be6996283c6c0c0a75fa975760
scottsmeester <scott@smeester.com>
2015-03-17 19:36:16 -0600

    heroku added. user profile functionality added

--- controllers/userMgmt.js
+++ controllers/userMgmt.js
@@ -5,7 +5,14 @@ var userMgmtController = {
     User.findOne({_id: req.user._id}, function(err, results){
       // console.log('_id:', req.user._id);
       // console.log(req.body);
-      res.send(req.body);
+      // var updatedUser = req.body;
+
+      // res.send(req.body);
+      var userId = req.user._id;
+
+      User.findByIdAndUpdate(userId, req.body, function(err, result){
+        res.send(result);
+      });
     });
   },
   edit: function(req, res){
