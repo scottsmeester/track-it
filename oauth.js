3e2022f8382b56775d5bc7556c31c44526a4a367
Scott Smeester <ssmeester@trueffect.com>
2015-05-06 11:39:31 -0600

    adding correct callback

--- oauth.js
+++ oauth.js
@@ -3,7 +3,7 @@ var ids = {
 salesforce: {
  consumerKey: '3MVG9fMtCkV6eLhce1fuNglis5pcdjkC9nqVFwg8_ra7Yx4Fvmq457iW7g_b0TOmxHlj9.zvrMq9CkeYbEUeV',
  consumerSecret: '4166713217352858701',
- callbackURL: "http://localhost:6403/auth/salesforce/callback"
+ callbackURL: "https://salestools.herokuapp.com/auth/salesforce/callback"
 }
 };
 
